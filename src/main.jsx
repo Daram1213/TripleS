c85e7c3157ce09c48e67278ed5e7b0f9a7ac7104
Sukki <knk951213@naver.com>
2023-11-17 19:35:18 +0900

    feat: header and navbar constructor setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Header from "./ui/components/Header";
-import "./input.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Header from './ui/components/Header'
+import './input.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import "../../input.css";
+import React from 'react'
+import '../../input.css'
+import navCategories from '../../assets/dummyData/dummyData'
 
 function Header() {
   return (
-    <div>
-      <h1 className="text-blue-500 font-bold">hi?</h1>
-    </div>
-  );
+    <>
+      <div className="main-hd-container bg-custom-bg relative w-screen h-96 bg-cover bg-no-repeat bg-center">
+        <div className="main-hd-and-navbar relative overflow-hidden px-8">
+          <div className="main-hd-inner max-x-9xl relative">
+            <div className="main-hd-flex-icon">
+              <div className="py-2">
+                <a className="text-white w-8 h-5" href="/">
+                  TripleS
+                </a>
+              </div>
+            </div>
+            <div className="main-hd-navbar-row">
+              <div className="main-hd-nav-con">
+                <ul className="main-hd-nav flex whitespace-nowrap">
+                  {navCategories.map((category) => (
+                    <li
+                      key={category.categoryCode}
+                      className="main-hd-nav-item inline-block float-none relative pr-8"
+                    >
+                      {' '}
+                      <a
+                        className="main-hd-nav-link text-white relative text-base inline-block h-5 pr-1 pb-1"
+                        href="/{category.link}"
+                        title="{category.title}"
+                      >
+                        {category.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Header;
+export default Header

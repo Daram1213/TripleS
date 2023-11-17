3e7cf06a251a0aa48e65ee83f57928c0a00153d1
Sukki <knk951213@naver.com>
2023-11-17 17:26:08 +0900

    feat: header components and constructor setting

---
+++ src/ui/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../input.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-blue-500 font-bold">hi?</h1>
+    </div>
+  );
+}
+
+export default Header;

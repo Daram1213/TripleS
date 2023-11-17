e363cb27b1bd258583cbe6eba6ea10011f7ec2bc
[SW7] 강동훈 <2023danielk@gmail.com>
2023-11-18 05:15:49 +0900

    host info UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,97 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FaDoorClosed } from 'react-icons/fa'
+import { PiMedalMilitary } from 'react-icons/pi'
+import { IoKeyOutline } from 'react-icons/io5'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex ml-3 max-w-lg">
+      <div className="host_info">
+        <div className="flex flex-row mb-2 mt-2 w-5/6 border-t-2 ">
+          <div className="host-container flex">
+            <div className="host-thumbnail">
+              <img
+                className="w-12 h-12 object-cover rounded-full mt-5 mb-5"
+                alt=""
+                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fCVFQyU4MiVBQyVFQiU5RSU4Q3xlbnwwfHwwfHx8MA%3D%3D"
+              />
+            </div>
+            <div className="host-profile flex flex-col ml-10 justify-center">
+              <div className="text-base font-bold">
+                <h3>호스트: a님 </h3>
+              </div>
+              <div className="text-sm ">
+                <div>호스트 경력 n년 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex content-evenly h-80 w-5/6 flex-wrap border-y-2">
+          <div className="host-items flex flex-col">
+            <div className="item1 flex flex-row mb-2 mt-2">
+              <div className="item1-container flex">
+                <div className="item1-icon m-2">
+                  <FaDoorClosed size="40" color="" />
+                </div>
+                <div className=" flex flex-col ml-10 justify-center">
+                  <div className="text-base font-bold">
+                    <h3 className="item1-title">셀프 체크인</h3>
+                  </div>
+                  <div className="text-sm">
+                    <div className="item1-contents">
+                      스마트 도어록을 이용해 체크인하세요.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-row mb-2 mt-2">
+              <div className="item2-container flex">
+                <div className="item2-icon m-2">
+                  <PiMedalMilitary size="40" color="" />
+                </div>
+
+                <div className=" flex flex-col ml-10 justify-center">
+                  <div className="text-base font-bold">
+                    <h3 className="item1-title">a님은 슈퍼호스트입니다</h3>
+                  </div>
+                  <div className="text-sm">
+                    <div className="item1-contents">
+                      슈퍼호스트는 풍부한 경험과 높은 평점을 자랑하는
+                      호스트입니다.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-row mb-2 mt-2">
+              <div className="item3-container flex">
+                <div className="item3-icon m-2">
+                  <IoKeyOutline size="40" color="" />
+                </div>
+
+                <div className=" flex flex-col ml-10 justify-center">
+                  <div className="text-base font-bold">
+                    <h3 className="item1-title">순조로운 체크인 과정</h3>
+                  </div>
+                  <div className="text-sm">
+                    <div className="item1-contents">
+                      최근 숙박한 게스트 중 100%가 체크인 과정에 별점 5점을 준
+                      숙소입니다.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=""></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 

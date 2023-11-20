d0677a4873fda674e2fea96f1611a56574108875
jay <valenqbd02@gmail.com>
2023-11-20 12:47:04 +0900

    chore: commit before rebase main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import MiniCard from './MiniCard'
+import Test from './Test'
 
 function App() {
   return (
     <>
-      <MiniCard />
+      <Test />
     </>
   )
 }
---
+++ src/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Test = () => {
+  return <div className="ml-10">테스트입</div>
+}
+
+export default Test

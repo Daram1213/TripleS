e5f38c4b7f765d76e339f37bc1d75a05b5005610
hido <bestdohee@gmail.com>
2023-11-22 16:53:07 +0900

    fix: 더미 데이터 변경

--- src/pages/detail/RoomComponent.jsx
+++ src/pages/detail/RoomComponent.jsx
@@ -3,23 +3,44 @@ import React from 'react'
 import { Box, Typography } from '@mui/material'
 import { Box, Typography } from '@mui/material'
 
-const RoomComponent = ({ room }) => {
+const RoomComponent = () => {
+  // 더미 데이터
+  const roomTypeData = {
+    name: 'Deluxe Sea View', // 객실 이름
+    types: 'Deluxe', // 유형
+    bedType: 'King Size', // 침대 유형
+    price: 200000, // 1박 요금
+    capacity: 2, // 수용 가능 인원
+    size: 35, // 방의 크기 (예: 평방미터)
+    image: [
+      'https://example.com/images/room1.jpg', // 객실 사진 URL
+      'https://example.com/images/room2.jpg',
+    ],
+    amenities: [
+      'Free WiFi', // 제공되는 편의 시설
+      'Air Conditioning',
+      'Mini Bar',
+    ],
+  }
+
   return (
     <Box className="max-w-sm rounded overflow-hidden shadow-lg mb-6">
       <Box className="px-6 py-4">
-        <Box className="font-bold text-xl mb-2">{room.name}</Box>
+        <Box className="font-bold text-xl mb-2">{roomTypeData.name}</Box>
         <Box className="text-gray-700 text-base">
           <Typography color="text.secondary">
-            {room.type} - {room.bedType}
+            {roomTypeData.type} - {roomTypeData.bedType}
           </Typography>
-          <Typography variant="body2">Capacity: {room.capacity}</Typography>
           <Typography variant="body2">
-            Size: {room.size} sqft, Floor: {room.floor}
+            Capacity: {roomTypeData.capacity}
           </Typography>
-          <Typography variant="body2">1박 ${room.price}</Typography>
+          <Typography variant="body2">
+            Size: {roomTypeData.size} sqft, Floor: {roomTypeData.floor}
+          </Typography>
+          <Typography variant="body2">1박 ${roomTypeData.price}</Typography>
         </Box>
       </Box>
-      <Box className="px-6 pt-4 pb-2">
+      {/* <Box className="px-6 pt-4 pb-2">
         {room.status ? (
           <Box className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
             예약 가능
@@ -27,55 +48,9 @@ const RoomComponent = ({ room }) => {
         ) : (
           <Box className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
             예약 불가
-    <Box className="max-w-sm rounded overflow-hidden shadow-lg mb-6">
-      <Box className="px-6 py-4">
-        <Box className="font-bold text-xl mb-2">{room.name}</Box>
-        <Box className="text-gray-700 text-base">
-          <Box className="max-w-sm rounded overflow-hidden shadow-lg mb-6">
-            <Box className="px-6 py-4">
-              <Box className="font-bold text-xl mb-2">{room.name}</Box>
-              <Box className="text-gray-700 text-base">
-                <Typography color="text.secondary">
-                  {room.type} - {room.bedType}
-                </Typography>
-                <Typography variant="body2">
-                  Capacity: {room.capacity}
-                </Typography>
-                <Typography variant="body2">
-                  Size: {room.size} sqft, Floor: {room.floor}
-                </Typography>
-                <Typography variant="body2">1박 ${room.price}</Typography>
-              </Box>
-            </Box>
-            <Box className="px-6 pt-4 pb-2">
-              {room.amenities.map((amenity, index) => (
-                <Box
-                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                  key={index}
-                >
-                  {amenity}
-                </Box>
-              ))}
-            </Box>
-            <Box className="px-6 pt-4 pb-2"></Box>
           </Box>
-          <Box className="px-6 pt-4 pb-2">
-            {room.status ? (
-              <Box className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                <Box className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                  예약 가능
-                </Box>
-              </Box>
-            ) : (
-              <Box className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                <Box className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                  예약 불가
-                </Box>
-              </Box>
-            )}
-          </Box>
-        </Box>
-      </Box>
+        )}
+      </Box> */}
     </Box>
   )
 }

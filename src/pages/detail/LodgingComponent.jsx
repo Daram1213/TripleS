f1bda8becd2560d18ab64b8fc327e861738ef0f5
hido <bestdohee@gmail.com>
2023-11-22 12:45:53 +0900

    fix: svg 파일 별도의 asset으로 분리

--- src/pages/detail/LodgingComponent.jsx
+++ src/pages/detail/LodgingComponent.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Typography, Grid } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import RoomComponent from './RoomComponent'
+import LeftArrowIcon from '../../assets/svg/LeftArrowIcon'
+import RightArrowIcon from '../../assets/svg/RightArrowIcon'
 
 const LodgingComponent = ({ lodging }) => {
   const [activeIndex, setActiveIndex] = useState(0)
@@ -82,21 +84,7 @@ const LodgingComponent = ({ lodging }) => {
           onClick={goToPrevSlide}
         >
           <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
+            <LeftArrowIcon />
 
             <span className="sr-only">Previous</span>
           </span>
@@ -108,21 +96,7 @@ const LodgingComponent = ({ lodging }) => {
           onClick={goToNextSlide}
         >
           <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
+            <RightArrowIcon />
 
             <span className="sr-only">Next</span>
           </span>

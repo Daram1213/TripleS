afd1a7a80e023051279b5a8094c9c83f334b85b6
[SW7] 강동훈 <2023danielk@gmail.com>
2023-11-22 16:14:19 +0900

    feat: implementing mui

--- src/pages/host_info/Item3.jsx
+++ src/pages/host_info/Item3.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { Box, Container, Typography } from '@mui/material'
 import { IoKeyOutline } from 'react-icons/io5'
 
 function Item3({ title, contents }) {
   return (
-    <div className="item3">
-      <div className="item3 flex flex-row mb-2 mt-2">
-        <div className="item3-container flex">
-          <div className="item3-icon m-2">
+    <Box className="item3">
+      <Box className="item3 flex flex-row mb-2 mt-2">
+        <Box className="item3-container flex">
+          <Box className="item3-icon m-2">
             <IoKeyOutline size="40" color="" />
-          </div>
-          <div className=" flex flex-col ml-10 justify-center">
-            <div className="text-base font-bold">
+          </Box>
+          <Box className=" flex flex-col ml-10 justify-center">
+            <Typography className="text-base font-bold">
               <h3 className="item3-title">{title}</h3>
-            </div>
-            <div className="text-sm">
-              <div className="item3-contents">{contents}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </Typography>
+            <Typography className="text-sm">
+              <Box className="item3-contents">{contents}</Box>
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 

9be702d505f9497783fc02294e05624e99d13650
hido <bestdohee@gmail.com>
2023-11-22 11:40:18 +0900

    fix: outlet 구조로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Router from './router'
+import { Outlet } from 'react-router'
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import DetailPage from './pages/DetailPage'
+import SearchPage from './pages/SearchPage'
+import SearchDetailPage from './pages/SearchDetailPage'
 
 const App = () => {
   return (
-    <>
-      <Router />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Outlet />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/search-detail" element={<SearchDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

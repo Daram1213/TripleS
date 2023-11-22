4f876d49f8ab1acdd7a9d930b2efb0f319e4065f
김나영 <newpaper.nana0@gmail.com>
2023-11-23 01:26:15 +0900

    feat: conflict 해결

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/main/Header'
 import './index.css'
 import App from './App.jsx'
@@ -12,12 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Header />
-
     <Filtering />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Footer />} />
-      </Routes>
-    </BrowserRouter>
+    <Footer />
   </React.StrictMode>,
 )

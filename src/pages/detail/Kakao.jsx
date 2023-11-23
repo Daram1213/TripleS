ab6fcf083c8198cb173adebc5f02388296aefe0a
hido <bestdohee@gmail.com>
2023-11-23 20:53:31 +0900

    feat: detail map ui edit

--- src/pages/detail/Kakao.jsx
+++ src/pages/detail/Kakao.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
-import { Box } from '@mui/material'
+import { Grid, Box, Typography } from '@mui/material'
 
 const { kakao } = window
 
 const Kakao = ({ lodgingData }) => {
-  console.log(lodgingData.map.latitude)
   useEffect(() => {
     const container = document.getElementById('map')
     const options = {
@@ -15,16 +14,40 @@ const Kakao = ({ lodgingData }) => {
       level: 3,
     }
     const map = new kakao.maps.Map(container, options)
+
+    var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667)
+
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    })
+
+    marker.setMap(map)
   }, [lodgingData])
 
   return (
     <Box
-      id="map"
-      style={{
-        width: '500px',
-        height: '500px',
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
-    ></Box>
+    >
+      <Grid
+        sx={{
+          width: '480px',
+          height: '570px',
+        }}
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box className="font-bold text-xl mb-2">숙소 위치</Box>
+        <Box id="map" style={{ width: '500px', height: '500px' }}></Box>
+      </Grid>
+    </Box>
   )
 }
 

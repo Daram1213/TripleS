ab1dc033f713fa2da5cd3564d9437e34894b0b88
jay <valenqbd02@gmail.com>
2023-11-23 18:57:58 +0900

    fix: fix css, routing, feat: update sidebar style that clicked

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import { Outlet } from 'react-router'
 import SearchCard from '../../components/search/SearchCard'
 
 function SearchPage() {
   return (
-    <div>
+    <Box>
       <SearchCard />
-    </div>
+      <Outlet />
+    </Box>
   )
 }
 

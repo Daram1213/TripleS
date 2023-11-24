1da434d0a8b09f9e11998cf86575fb64867d79fa
김나영 <newpaper.nana0@gmail.com>
2023-11-24 19:47:14 +0900

    feat: Filtering ui 수정 및 input focus 기능

--- src/pages/main/Filtering/FilteringFuc/CheckInOut.jsx
+++ src/pages/main/Filtering/FilteringFuc/CheckInOut.jsx
@@ -1,11 +1,22 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo'
+import { DemoItem, DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { Box, Typography } from '@mui/material'
 
-export default function ResponsiveDatePickers() {
+export default function CheckInOut() {
+  const [checkInDate, setCheckInDate] = useState(dayjs('2022-04-17'))
+  const [checkOutDate, setCheckOutDate] = useState(dayjs('2022-04-17'))
+  const [nightCount, setNightCount] = useState(0)
+
+  // 날짜 변경에 따른 몇 박 몇 일 계산
+  useEffect(() => {
+    const nights = checkOutDate.diff(checkInDate, 'day')
+    setNightCount(nights)
+  }, [checkInDate, checkOutDate])
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer
@@ -16,9 +27,32 @@ export default function ResponsiveDatePickers() {
           'StaticDatePicker',
         ]}
       >
-        <DemoItem label="체크인">
-          <DatePicker defaultValue={dayjs('2022-04-17')} />
-        </DemoItem>
+        <Box className="flex">
+          <Box>
+            <DemoItem label="체크인">
+              <DatePicker
+                value={checkInDate}
+                onChange={(date) => setCheckInDate(date)}
+              />
+            </DemoItem>
+          </Box>
+
+          <Box>
+            <Typography className="text-xs">
+              {nightCount} 박 {nightCount + 1} 일
+            </Typography>
+          </Box>
+
+          <Box className="ml-2">
+            <DemoItem label="체크아웃">
+              <DatePicker
+                value={checkOutDate}
+                onChange={(date) => setCheckOutDate(date)}
+                minDate={checkInDate.add(1, 'day')} // 체크인 날짜 이후의 날짜만 선택 가능하도록 설정
+              />
+            </DemoItem>
+          </Box>
+        </Box>
       </DemoContainer>
     </LocalizationProvider>
   )

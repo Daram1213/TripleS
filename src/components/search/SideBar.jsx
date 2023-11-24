7584c9e4bcf167796000c63bd0d1aeea24fc8586
jay <valenqbd02@gmail.com>
2023-11-24 10:18:11 +0900

    chore: change btns in sidebar

--- src/components/search/SideBar.jsx
+++ src/components/search/SideBar.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { useLocation, Link } from 'react-router-dom'
 import { Typography } from '@mui/material'
 import { Button } from '@mui/base'
+<<<<<<< refs/remotes/origin/main
 
 function SideBar({ keyword }) {
   const { pathname } = useLocation()
@@ -102,6 +103,8 @@ import { useEffect } from 'react'
 import { useLocation, Link } from 'react-router-dom'
 >>>>>>> fix: fix css, routing, feat: update sidebar style that clicked
 import { Typography } from '@mui/material'
+=======
+>>>>>>> chore: change btns in sidebar
 
 function SideBar({ keyword }) {
   const { pathname } = useLocation()
@@ -133,7 +136,7 @@ function SideBar({ keyword }) {
   return (
     <nav className="min-w-[250px] relative my-4 flex gap-1 overflow-x-auto p-2 font-sans text-base font-normal text-gray-700 sm:flex-row md:flex-row lg:flex-col">
       <Link to={`/searchList/${keyword}`}>
-        <button
+        <Button
           tabIndex={-1}
           className="btn btn-all min-w-90 flex h-12 items-center rounded-lg border border-solid border-gray-400 py-2 pl-6 text-start text-gray-900"
         >
@@ -144,10 +147,10 @@ function SideBar({ keyword }) {
           >
             전체 (12,6372)
           </Typography>
-        </button>
+        </Button>
       </Link>
       <Link to={`/hotelSearchList/${keyword}`}>
-        <button
+        <Button
           tabIndex={-1}
           className="btn btn-hotel min-w-90 flex h-12 items-center rounded-lg border border-solid border-gray-400 py-2 pl-6 text-start text-gray-900"
         >
@@ -158,10 +161,10 @@ function SideBar({ keyword }) {
           >
             호텔 (12,6372)
           </Typography>
-        </button>
+        </Button>
       </Link>
       <Link to={`/tourSearchList/${keyword}`}>
-        <button
+        <Button
           tabIndex={-1}
           className="btn btn-tour min-w-90 flex h-12 items-center rounded-lg border border-solid border-gray-400 py-2 pl-6 text-start text-gray-900"
         >
@@ -172,10 +175,10 @@ function SideBar({ keyword }) {
           >
             여행지 (12,6372)
           </Typography>
-        </button>
+        </Button>
       </Link>
       <Link to={`/commentList/${check}`}>
-        <button
+        <Button
           tabIndex={-1}
           className="min-w-90 flex h-12 items-center rounded-lg border border-solid border-gray-400 py-2 pl-6 text-start active:bg-blue-600 active:text-gray-50"
         >
@@ -186,7 +189,7 @@ function SideBar({ keyword }) {
           >
             코멘트 (12,6372)
           </Typography>
-        </button>
+        </Button>
       </Link>
     </nav>
   )

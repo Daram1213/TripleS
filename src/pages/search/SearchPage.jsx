fc1c943a262673ac2635dec51165a6a74b33f472
jay <valenqbd02@gmail.com>
2023-11-24 17:22:27 +0900

    chore: fix error rebase main

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,13 +1,23 @@
+<<<<<<< HEAD
 import { Box } from '@mui/material'
 import { Outlet } from 'react-router'
+=======
+import React from 'react'
+>>>>>>> bedbb3f (feat: create search main page, card UI, sidebar)
 import SearchCard from '../../components/search/SearchCard'
 
 function SearchPage() {
   return (
+<<<<<<< HEAD
     <Box>
       <SearchCard />
       <Outlet />
     </Box>
+=======
+    <div>
+      <SearchCard />
+    </div>
+>>>>>>> bedbb3f (feat: create search main page, card UI, sidebar)
   )
 }
 

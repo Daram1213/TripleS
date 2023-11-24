0d7e92d5c17b98a0afdc1d1558e8e23b9d031f11
jay <valenqbd02@gmail.com>
2023-11-24 17:25:22 +0900

    chore: fix error rebase main2

--- src/pages/search/SearchDetailPage.jsx
+++ src/pages/search/SearchDetailPage.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { Box } from '@mui/material'
 
 function SearchDetailPage() {
   return <Box>검색 더보기 페이지</Box>
-=======
-import React from 'react'
-
-const SearchDetailPage = () => {
-  return <div>검색 더보기 페이지</div>
->>>>>>> bedbb3f (feat: create search main page, card UI, sidebar)
 }
 
 export default SearchDetailPage

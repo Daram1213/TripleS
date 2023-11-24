f85b8df2425ddc87843799f5a05c67fa185882a8
jay <valenqbd02@gmail.com>
2023-11-24 14:13:04 +0900

    chore: change div to Box in searchDetail

--- src/pages/search/SearchDetailPage.jsx
+++ src/pages/search/SearchDetailPage.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Box } from '@mui/material'
 
 function SearchDetailPage() {
+<<<<<<< refs/remotes/origin/main
   return <div>검색 더보기 페이지</div>
+=======
+  return <Box>검색 더보기 페이지</Box>
+>>>>>>> chore: change div to Box in searchDetail
 }
 
 export default SearchDetailPage

f4244414b599a90cb84b7b8f3c9a4efe613c8377
김나영 <newpaper.nana0@gmail.com>
2023-11-24 22:35:16 +0900

    feat: filtering conflict 해결

--- src/pages/main/Filtering/Hotel.jsx
+++ src/pages/main/Filtering/Hotel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Box, TextField } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 // import { IoIosInformationCircle, IoMdStarOutline } from 'react-icons/io'
 import { IoSearch } from 'react-icons/io5'
 import RoomCount from './FilteringFuc/RoomCount'
@@ -18,30 +18,8 @@ function Hotel() {
         <CheckInOut />
       </Box>
 
-      <Box className="flex items-center justify-center p-3 h-fit">
-        <Typography className="text-xs">1</Typography>
-      </Box>
-
-      <Box className="border-grey rounded-b rounded-r w-8/12 h-fit border p-2 border-b border-l border-grey rounded-l ml-2">
-        <Box className="flex">
-          <Typography className="text-xs">체크아웃</Typography>
-        </Box>
-        <Box>
-          <TextField className="p-3 w-full outline-none text-sm border-b border-blue-500 focus:border-blue-500" />
-        </Box>
-      </Box>
-
       <Box className="ml-2 border-t border-b border-l border-r border-grey rounded-l w-7/12">
         <RoomCount />
-        {/* <Box className="flex text-grey-700 p-3">
-          <Typography className="text-xs">객실당 인원 수</Typography>
-        </Box>
-        <Box>
-          <input
-            className="focus:border-b-2 focus:border-blue-500 p-3 w-full outline-none text-sm"
-            placeholder="객실 1개, 성인 2명, 어린이 0명"
-          />
-        </Box> */}
       </Box>
 
       {/* 검색 버튼 */}

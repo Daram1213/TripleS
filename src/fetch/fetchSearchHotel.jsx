4a47a6b77d49d129ffca3a053eb9592552d9127e
jay <valenqbd02@gmail.com>
2023-11-24 23:52:01 +0900

    chore: set query key page default 1

--- src/fetch/fetchSearchHotel.jsx
+++ src/fetch/fetchSearchHotel.jsx
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const fetchSearchHotel = async ({ queryKey }) => {
   const keyword = queryKey[1]
-  const items = queryKey[2] ?? 1
+  const items = queryKey[2]
   // 한 페이지에 보여줄 아이템 수
-  const page = queryKey[3]
+  const page = queryKey[3] ?? 1
 
   const apiRes = await axios.get(
     `http://15.165.25.34:3000/api/search?keyword=%EC%84%9C%EC%9A%B8&type=lodging&page=1&item=${items}`,

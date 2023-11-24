cf498c95152f8b2b34a2c916c5a1af5a33f77884
jay <valenqbd02@gmail.com>
2023-11-24 14:19:21 +0900

    chore: rebase main fix conflict

--- src/components/search/SearchCard.jsx
+++ src/components/search/SearchCard.jsx
@@ -211,6 +211,46 @@ function SearchCard() {
                 </Typography>
               </Link>
             </Box>
+<<<<<<<
+
+=======
+=======
+  const { search } = useParams()
+
+  // useQuery로 데이터 fetch comments는 테스트용
+  // 종합 결과 페이지에서는 페이지 데이터 x로 1페이지만 불러옴
+  // 현재 더미데이터 tours, hotels쓰고 있어 쓰지 않음.
+  const hotelList = useQuery(['hotels', search], fetchHotel)
+  const tourList = useQuery(['tours', search], fetchTour)
+
+  return (
+    // container
+    <Box className="container flex justify-center">
+      <Box className="max-w-[calc(100vw - 64px)] flex w-[1350px] overflow-x-auto sm:flex-col md:flex-col lg:flex-row">
+        {/* sidebar에 useParams로 가져온 search를 props로 전달 */}
+        <SideBar search={search} />
+        <Box className="flex flex-auto flex-col px-4">
+          <Box className="content-container hotel-container flex-auto px-4">
+            <Box className="my-5">
+              <h2 className="text-2xl font-bold text-gray-900">호텔</h2>
+            </Box>
+            <Box className="grid w-full max-w-6xl  sm:grid-cols-1 sm:gap-x-6 md:grid-cols-2 lg:grid-cols-3">
+              {!hotels.length ? (
+                <h1>호텔 정보가 없습니다.</h1>
+              ) : (
+                hotels.map((hotel) => (
+                  <HotelItem key={hotel.id} hotel={hotel} />
+                ))
+              )}
+            </Box>
+            <Link to={`/hotelList/${search}`}>
+              <Box className="flex items-end justify-end">
+                <Typography className="text-lg text-gray-500">
+                  더보기
+                </Typography>
+              </Box>
+            </Link>
+>>>>>>>
           </Box>
         </Box>
       </Box>

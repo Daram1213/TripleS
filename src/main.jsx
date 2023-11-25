643d42434a2fa4195c4ad12a7e0135951d058f0c
DongHoun Kang <2023danielk@gmail.com>
2023-11-25 22:51:08 +0900

    rebase를 위한 커밋 9번째

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+<<<<<<< HEAD
+=======
+import Header from './pages/main/Header'
+import App from './App.jsx'
+>>>>>>> e2a07f0 (chore:amending Header import directory from components to pages)
 import './index.css'
 import App from './App'
 import HostInfo from './pages/host_info/HostInfo'

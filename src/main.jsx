4b464a43676d7a125e7ffe27674483c6c7a6ec86
DongHoun Kang <2023danielk@gmail.com>
2023-11-25 23:52:01 +0900

    chore: commit for pull request

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-// import App from './App'
-import Hostinfo from './Hostinfo'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Hostinfo />
+    <App />
   </React.StrictMode>,
 )

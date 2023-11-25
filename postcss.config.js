54a55713ca12f01d67f91ce4a95879b2dfc1237a
DongHoun Kang <2023danielk@gmail.com>
2023-11-25 22:49:24 +0900

    rebase를 위한 커밋 7번째

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-/* eslint-disable import/no-unresolved */
-/* eslint global-require: off, import/no-extraneous-dependencies: off */
-module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
-=======
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
->>>>>>> e363cb2 (host info UI completed)
 }

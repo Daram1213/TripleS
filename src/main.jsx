87ca8af0868aa1c3eeb01044c916736176e4ff55
DongHoun Kang <2023danielk@gmail.com>
2023-11-26 01:01:07 +0900

    chore: organise folder tree

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+// import App from './App'
+import Review from './Review'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Review />
   </React.StrictMode>,
 )

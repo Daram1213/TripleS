2e83899366b90045f81813adee12a4f103942960
DongHoun Kang <2023danielk@gmail.com>
2023-11-26 01:01:07 +0900

    chore: organise folder tree

---
+++ src/pages/detail/review/Evaluation3Component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box, Container, Typography } from '@mui/material'
+import { LiaBroomSolid } from 'react-icons/lia'
+import { IoIosCheckmarkCircleOutline } from 'react-icons/io'
+import { IoKeyOutline } from 'react-icons/io5'
+import { SlSpeech } from 'react-icons/sl'
+import { CiMap } from 'react-icons/ci'
+import { GiPayMoney } from 'react-icons/gi'
+
+function Evaluation3Component({ name, rate }) {
+  return (
+    <Box className="flex flex-col ml-2 border-r-2">
+      <Box className="evaluation1 w-20 ml-2">
+        <Box className="evaluation1-name text-xs "> {name}</Box>
+        <Box className="evaluation1-rate font-bold text-sm"> {rate}</Box>
+        <Box className="evaluation1-icon mt-5">
+          <IoKeyOutline size="40" />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Evaluation3Component

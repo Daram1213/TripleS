71cce8f8fe92bfd7647d5a25b7611fed19916d15
hido <bestdohee@gmail.com>
2023-11-28 00:33:32 +0900

    fix: 불필요한 코드 정리

--- src/pages/detail/ReservationModal.jsx
+++ src/pages/detail/ReservationModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
 import { Box, Typography, Button } from '@mui/material'
-import { Box, Typography, Button } from '@mui/material'
 import CloseIcon from '../../assets/svg/CloseIcon'
 
 const ReservationModal = ({ lodgingData, closeModal, reservations }) => {
@@ -61,115 +60,83 @@ const ReservationModal = ({ lodgingData, closeModal, reservations }) => {
         className="bg-white p-4 md:p-8 rounded-lg shadow-2xl max-w-md mx-auto"
         style={{ borderRadius: 10 }}
       >
-        <Box
-          className="bg-white p-4 md:p-8 rounded-lg shadow-2xl max-w-md mx-auto"
-          style={{ borderRadius: 10 }}
-        >
-          <Box className="text-center space-y-4">
-            <Typography className="text-3xl font-bold text-gray-900">
-              ₩{pricePerNight.toLocaleString()} /박
-            </Typography>
-            <Typography
-              variant="h6"
-              className="font-bold text-gray-900"
-              style={{ fontWeight: 'bold' }}
-            >
-              예약 요청
-            </Typography>
+        <Box className="text-center space-y-4">
+          <Typography
+            variant="h6"
+            className="font-bold text-gray-900"
+            style={{ fontWeight: 'bold' }}
+          >
+            예약 요청
+          </Typography>
 
-            <Box className="flex items-center">
-              <img
-                src={lodgingData.image[0]}
-                alt="객실 이미지"
-                className="max-w-full mx-auto rounded-lg"
-                style={{ maxWidth: '30%', height: 'auto', borderRadius: 10 }}
-              />
-              <Typography className="text-3xl font-bold text-gray-900 ml-4">
-                {lodgingData.name}, {lodgingData.address}
-              </Typography>
-            </Box>
-
-            <Box className="flex items-center">
-              <img
-                src={lodgingData.image[0]}
-                alt="객실 이미지"
-                className="max-w-full mx-auto rounded-lg"
-                style={{ maxWidth: '30%', height: 'auto', borderRadius: 10 }}
-              />
-              <Typography className="text-3xl font-bold text-gray-900 ml-4">
-                {lodgingData.name}, {lodgingData.address}
-              </Typography>
-            </Box>
-            <Box className="space-y-2">
-              <Box>
-                <Typography
-                  className="text-gray-800"
-                  style={{ fontWeight: 'bold' }}
-                >
-                  날짜
-                </Typography>
-                <Typography
-                  className="text-gray-800"
-                  style={{ fontWeight: 'bold' }}
-                >
-                  날짜
-                </Typography>
-                <Typography className="text-gray-800">
-                  {checkInDate.format('YYYY.MM.DD')}~
-                  {checkOutDate.format('YYYY.MM.DD')}
-                </Typography>
-              </Box>
-              <Box>
-                <Typography
-                  className="text-gray-800"
-                  style={{ fontWeight: 'bold' }}
-                >
-                  인원
-                </Typography>
-                <Typography
-                  className="text-gray-800"
-                  style={{ fontWeight: 'bold' }}
-                >
-                  인원
-                </Typography>
-                <Typography className="text-gray-800">
-                  성인 {reservations.adults} / 아이{reservations.children}
-                  성인 {roomBookingData.adults} / 아이{roomBookingData.children}
-                </Typography>
-              </Box>
-            </Box>
-            {!isReserved ? (
-              <Button
-                onClick={handleReservation}
-                variant="contained"
-                color="primary"
-                className="w-full rounded-full transition duration-300 ease-in-out hover:bg-pink-700"
-              >
-                예약하기
-              </Button>
-            ) : (
-              <Box className="text-lg text-green-700 bg-green-100 py-2 px-4 rounded-md border border-green-200">
-                예약되었습니다!
-              </Box>
-            )}
-            <Typography className="text-sm text-gray-500">
-              예약 확정 전에는 요금이 청구되지 않습니다.
+          <Box className="flex items-center">
+            <img
+              src={lodgingData.image[0]}
+              alt="객실 이미지"
+              className="max-w-full mx-auto rounded-lg"
+              style={{ maxWidth: '30%', height: 'auto', borderRadius: 10 }}
+            />
+            <Typography className="text-3xl font-bold text-gray-900 ml-4">
+              {lodgingData.name}, {lodgingData.address}
             </Typography>
-            <Box className="pt-4 border-t-2">
-              <Typography className="text-lg font-semibold">
-                ₩{pricePerNight.toLocaleString()} x {totalNights}박
-              </Typography>
-              <Typography className="text-xl font-bold">
-                총합계 ₩{totalPrice.toLocaleString()}
-              </Typography>
-            </Box>
-            <button
-              onClick={closeModal}
-              className="absolute top-0 right-0 mt-4 mr-4 text-gray-400 hover:text-gray-600"
-            >
-              <CloseIcon />
-            </button>
           </Box>
+
+          <Box className="space-y-2">
+            <Box>
+              <Typography
+                className="text-gray-800"
+                style={{ fontWeight: 'bold' }}
+              >
+                날짜
+              </Typography>
+              <Typography className="text-gray-800">
+                {checkInDate.format('YYYY.MM.DD')}~
+                {checkOutDate.format('YYYY.MM.DD')}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                className="text-gray-800"
+                style={{ fontWeight: 'bold' }}
+              >
+                인원
+              </Typography>
+              <Typography className="text-gray-800">
+                성인 {roomBookingData.adults} / 아이{roomBookingData.children}
+              </Typography>
+            </Box>
+          </Box>
+          {!isReserved ? (
+            <Button
+              onClick={handleReservation}
+              variant="contained"
+              color="primary"
+              className="w-full rounded-full transition duration-300 ease-in-out hover:bg-pink-700"
+            >
+              예약하기
+            </Button>
+          ) : (
+            <Box className="text-lg text-green-700 bg-green-100 py-2 px-4 rounded-md border border-green-200">
+              예약되었습니다!
+            </Box>
+          )}
+          <Typography className="text-sm text-gray-500">
+            예약 확정 전에는 요금이 청구되지 않습니다.
+          </Typography>
+          <Box className="pt-4 border-t-2">
+            <Typography className="text-lg font-semibold">
+              ₩{pricePerNight.toLocaleString()} x {totalNights}박
+            </Typography>
+            <Typography className="text-xl font-bold">
+              총합계 ₩{totalPrice.toLocaleString()}
+            </Typography>
+          </Box>
+          <button
+            onClick={closeModal}
+            className="absolute top-0 right-0 mt-4 mr-4 text-gray-400 hover:text-gray-600"
+          >
+            <CloseIcon />
+          </button>
         </Box>
       </Box>
     </Box>

a764c8339693bb0b57bc890e3e8e2f2765e9a2a7
sukkiii <122197644+Sukkiii@users.noreply.github.com>
2023-11-29 20:33:18 +0900

    feature: mainpage detail (#37)
    * feature: mainpage 검색창 이동, page scrollupdown, hotel페이지 생성
    
    * fix: tailwind style change, unnecessary setting change
    
    * fix: unnecessary setting change

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import { ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import MainPage from './pages/MainPage'
-import HotelDetailPage from './pages/HotelDetailPage'
+import HotelPage from './pages/main/HotelPage/HotelPage'
+import DetailPage from './pages/DetailPage'
 import SearchPage from './pages/search/SearchPage'
-import SearchDetailPage from './pages/search/SearchDetailPage'
 import SearchHotelList from './pages/search/SearchHotelList'
 import SearchTourList from './pages/search/SearchTourList'
-import ReservationPage from './pages/reservation/ReservationPage'
 
 function App() {
   const theme = createTheme()
@@ -26,30 +25,29 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route element={<Outlet />}>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/detail/:lodgingId" element={<HotelDetailPage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/search-detail" element={<SearchDetailPage />} />
-              {/* 다른 중첩된 라우트 설정 */}
-              <Route
-                path="/searchHotelList/:keyword"
-                element={<SearchHotelList />}
-              />
-              <Route
-                path="/searchTourList/:keyword"
-                element={<SearchTourList />}
-              />
-              <Route path="/search-detail" element={<SearchDetailPage />} />
-              <Route path="/reservation" element={<ReservationPage />} />
-            </Route>
-          </Routes>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <CssBaseline>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route element={<Outlet />}>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/hotel" element={<HotelPage />} />
+                {/* 통합검색, 호텔리스트, 여행지리스트 */}
+                <Route path="/searchList/:keyword" element={<SearchPage />} />
+                <Route
+                  path="/searchHotelList/:keyword"
+                  element={<SearchHotelList />}
+                />
+                <Route
+                  path="/searchTourList/:keyword"
+                  element={<SearchTourList />}
+                />
+                <Route path="/hotelDetail" element={<DetailPage />} />
+              </Route>
+            </Routes>
+          </QueryClientProvider>
+        </BrowserRouter>
+      </CssBaseline>
     </ThemeProvider>
   )
 }

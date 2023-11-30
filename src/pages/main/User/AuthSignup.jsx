969d04ec1575be30aadbfee09e9c4bf5425db2b2
김나영 <newpaper.nana0@gmail.com>
2023-12-01 02:21:55 +0900

    feat: css 수정 및 삭제 & api test

--- src/pages/main/User/AuthSignup.jsx
+++ src/pages/main/User/AuthSignup.jsx
@@ -9,15 +9,17 @@ function AuthSignup() {
   const [birth, setBirth] = useState('')
   const [address, setAddress] = useState('')
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     try {
       // 회원가입에 사용할 데이터
       const userData = {
-        email: 'email',
-        userName: 'name',
-        password: 'password',
-        address: 'address',
-        birthDay: 'birth',
+        email,
+        name,
+        password,
+        address,
+        userId: 123,
+        isAdmin: false,
       }
 
       // axios.post 메서드를 사용하여 POST 요청 보내기
@@ -31,32 +33,45 @@ function AuthSignup() {
         },
       )
 
+      // if (response?.data?.success) {
+
+      // } else if (response?.data?.error) {
+      //   alert(response.data.error)
+      //   alert('회원가입 실패')
+      // }
       // 응답 데이터 출력
-      console.log('signUp', response.data)
+      // console.log('signUp', response.data)
     } catch (error) {
       // 에러 발생 시 처리
-      console.log('signUp failed', error)
+      // console.log('signUp failed', error)
     }
   }
 
   return (
-    <Box className="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-400 bg-white shadow-lg p-8 rounded h-max">
-      <Box>
-        <Typography
-          display="flex"
-          alignItems="flex-start"
-          id="modal-modal-title"
-          variant="h6"
-          fontWeight="bold"
-        >
-          회원가입
-        </Typography>
-      </Box>
-      <Box className="mt-2">
-        <Typography variant="caption" color="textSecondary">
-          간편하게 예약을 관리하고 회원 전용 혜택도 누려보세요
-        </Typography>
-      </Box>
+    <Box className="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-400 bg-white shadow-lg p-8 rounded h-max gap-2">
+      <Typography
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '-1rem',
+          fontWeight: 'bold',
+          cursor: 'pointer',
+        }}
+      >
+        X
+      </Typography>
+      <Typography
+        display="flex"
+        alignItems="flex-start"
+        id="modal-modal-title"
+        variant="h6"
+        fontWeight="bold"
+      >
+        회원가입
+      </Typography>
+      <Typography variant="caption" color="textSecondary">
+        간편하게 예약을 관리하고 회원 전용 혜택도 누려보세요
+      </Typography>
       <Box
         className="mt-6 w-full"
         component="form"
@@ -147,12 +162,10 @@ function AuthSignup() {
           로그인
         </Typography>
       </Box>
-      <Box className="mt-6">
-        <Typography variant="caption" color="textSecondary">
-          로그인 또는 회원가입 시, TripleS 이용약관 및 개인정보 정책에 동의한
-          것으로 간주합니다.
-        </Typography>
-      </Box>
+      <Typography variant="caption" color="textSecondary">
+        로그인 또는 회원가입 시, TripleS 이용약관 및 개인정보 정책에 동의한
+        것으로 간주합니다.
+      </Typography>
     </Box>
   )
 }

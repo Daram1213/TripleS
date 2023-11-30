0de081413180c6cd92f8b2621aa4a9d24cac66c3
김나영 <newpaper.nana0@gmail.com>
2023-12-01 02:21:55 +0900

    feat: css 수정 및 삭제 & api test

--- src/pages/main/User/AuthLogin.jsx
+++ src/pages/main/User/AuthLogin.jsx
@@ -4,21 +4,29 @@ import { Box, Typography, TextField, Button, Divider } from '@mui/material'
 import { RiKakaoTalkFill } from 'react-icons/ri'
 import { FcGoogle } from 'react-icons/fc'
 import { SiNaver } from 'react-icons/si'
+// import { useHistory } from 'react-router-dom'
 // import AuthSignup from './AuthSignup'
 
 function AuthLogin() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = async () => {
+  // const history = useHistory()
+
+  // const handleNavigate = () => {
+  //   history.push('/') // 홈으로 이동
+  // }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     try {
       const userData = {
-        email: 'email',
-        password: 'password',
+        email: 'seodnjstlr@gmail.com',
+        password: '12345678',
       }
 
       const response = await axios.post(
-        'http://15.165.25.34:3000/api/users/login',
+        'http://15.165.25.34:3000/api/login',
         userData,
         {
           headers: {
@@ -26,13 +34,29 @@ function AuthLogin() {
           },
         },
       )
-      console.log('login', response.data)
+      // const accessToken = response.data.token
+
+      if (!response.statusText) {
+        throw new Error('에러.')
+      }
+      // console.log('login', response)
     } catch (error) {
-      console.log('login failed', error)
+      // console.log('login failed', error)
     }
   }
   return (
-    <Box className="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-400 bg-white shadow-lg p-8 rounded h-3/4">
+    <Box className="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-400 bg-white shadow-lg p-6 rounded h-4/5 gap-2">
+      <Typography
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '-1rem',
+          fontWeight: 'bold',
+          cursor: 'pointer',
+        }}
+      >
+        X
+      </Typography>
       <Typography
         display="flex"
         alignItems="flex-start"
@@ -42,13 +66,11 @@ function AuthLogin() {
       >
         로그인
       </Typography>
-      <Box className="mt-2">
-        <Typography variant="caption" color="textSecondary">
-          간편하게 예약을 관리하고 회원 전용 혜택도 누려보세요
-        </Typography>
-      </Box>
+      <Typography variant="caption" color="textSecondary">
+        간편하게 예약을 관리하고 회원 전용 혜택도 누려보세요
+      </Typography>
       <Box
-        className="mt-6 w-full"
+        className="mt-6 w-full flex flex-col gap-2"
         component="form"
         noValidate
         autoComplete="off"
@@ -65,31 +87,27 @@ function AuthLogin() {
             setEmail(e.target.value)
           }}
         />
-        <Box className="mt-2">
-          <TextField
-            type="password"
-            value={password}
-            className="w-full"
-            id="outlined-basic"
-            label="비밀번호"
-            placeholder="비밀번호를 입력해주세요"
-            variant="outlined"
-            onChange={(e) => {
-              setPassword(e.target.value)
-            }}
-          />
-        </Box>
-        <Box className="mt-6 w-full">
-          <Button
-            type="submit"
-            onClick={handleSubmit}
-            variant="contained"
-            className="w-full h-12"
-            sx={{ backgroundColor: 'rgba(0, 0, 255, 0.5)' }}
-          >
-            로그인
-          </Button>
-        </Box>
+        <TextField
+          type="password"
+          value={password}
+          className="w-full"
+          id="outlined-basic"
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요"
+          variant="outlined"
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        />
+        <Button
+          type="submit"
+          onClick={handleSubmit}
+          variant="contained"
+          className="w-full h-12"
+          sx={{ backgroundColor: 'rgba(0, 0, 255, 0.5)' }}
+        >
+          로그인
+        </Button>
       </Box>
       <Box className="flex justify-center mt-2">
         <Typography
@@ -134,12 +152,10 @@ function AuthLogin() {
           <FcGoogle class="text-4xl ml-6 rounded-full bg-white-500 p-2 border cursor-pointer" />
         </Typography>
       </Box>
-      <Box>
-        <Typography variant="caption" color="textSecondary">
-          로그인 또는 회원가입 시, TripleS 이용약관 및 개인정보 정책에 동의한
-          것으로 간주합니다.
-        </Typography>
-      </Box>
+      <Typography variant="caption" color="textSecondary">
+        로그인 또는 회원가입 시, TripleS 이용약관 및 개인정보 정책에 동의한
+        것으로 간주합니다.
+      </Typography>
     </Box>
   )
 }

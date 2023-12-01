dcbce03ed65a4b4f22d7f8a81f71a4a99b1f0b89
DongHoun Kang <2023danielk@gmail.com>
2023-12-01 23:58:08 +0900

    feat: review data api로 받아오기 구현중...

--- src/pages/detail/review/ReviewCardComponent.jsx
+++ src/pages/detail/review/ReviewCardComponent.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Box, Container, Typography } from '@mui/material'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { LuDot } from 'react-icons/lu'
 
-function ReviewCardComponent({ name, visitDate }) {
+function ReviewCardComponent({ reviewData }) {
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch('http://15.165.25.34:3000/api/lodgings/1')
+        const data = await response.json()
+        setReviews(data)
+      } catch (error) {
+        console.error('리뷰 호출 중 에러 발생', error)
+      }
+    }
+
+    fetchReviews()
+  }, [])
+
   return (
     <Box className="Reviewcard1 mb-5 w-96">
+      {reviews.map((review) => {})}
       <Box className="flex flex-row w-96">
         <Box className="visitor-thumbnail">
           <img
@@ -15,17 +32,18 @@ function ReviewCardComponent({ name, visitDate }) {
           />
         </Box>
         <Box className="host-profile flex flex-col ml-2 justify-center">
-          <Box className="text-sm font-bold">{name}</Box>
+          <Box className="text-sm font-bold">{reviews.user}</Box>
           <Box className="text-xs">서울,대한민국</Box>
         </Box>
       </Box>
       <Box className="flex flex-row">
-        <Box className="rate_star text-xs">별점</Box>
+        <Box className="rate_star text-xs">{reviews.rating}</Box>
         <LuDot />
-        <Box className="visitDate text-xs">{visitDate}</Box>
+        <Box className="visitDate text-xs">{createdAt}</Box>
       </Box>
 
       <Box className="review_comment text-xs w-8/12 ">
+        {reviews.content}
         이태원의 조용한 주택가에 위치한 아름다운 아파트입니다. 모든 방향의
         지역에 쉽게 접근할 수 있으며, 성인 4명이 일주일 장기 숙박에 정말
         편안하게 머물 수 있습니다.

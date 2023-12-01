d8ee34f8cb2e57db73fd4bc9ef70f4c2fa1adf45
김나영 <newpaper.nana0@gmail.com>
2023-12-01 22:42:37 +0900

    feat: 로그인&회원가입 전환되게 만듦

--- src/pages/main/User/AuthSignup.jsx
+++ src/pages/main/User/AuthSignup.jsx
@@ -65,23 +65,12 @@ function AuthSignup() {
   }
 
   return (
-    <Box className="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/5 bg-white shadow-lg p-8 rounded h-max gap-2">
-      <Typography
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          marginTop: '-1rem',
-          fontWeight: 'bold',
-          cursor: 'pointer',
-        }}
-      >
-        X
-      </Typography>
+    <Box className="flex flex-col absolute p-6 rounded h-fit gap-2">
       <Typography
         display="flex"
         alignItems="flex-start"
         id="modal-modal-title"
-        variant="h6"
+        variant="h5"
         fontWeight="bold"
       >
         회원가입
@@ -90,7 +79,7 @@ function AuthSignup() {
         간편하게 예약을 관리하고 회원 전용 혜택도 누려보세요
       </Typography>
       <Box
-        className="flex flex-col mt-6 w-full gap-2"
+        className="flex flex-col w-full gap-2"
         component="form"
         noValidate
         autoComplete="off"
@@ -171,17 +160,11 @@ function AuthSignup() {
           회원가입
         </Button>
       </Box>
-
-      <Box className="flex justify-center mt-2">
-        <Typography
-          variant="body2"
-          color="textSecondary"
-          sx={{ cursor: 'pointer' }}
-        >
-          로그인
-        </Typography>
-      </Box>
-      <Typography variant="caption" color="textSecondary">
+      <Typography
+        sx={{ marginTop: '5rem' }}
+        variant="caption"
+        color="textSecondary"
+      >
         로그인 또는 회원가입 시, TripleS 이용약관 및 개인정보 정책에 동의한
         것으로 간주합니다.
       </Typography>

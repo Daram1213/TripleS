2c9d09218efe35120740a2581c91fdfd8daf0553
hido <bestdohee@gmail.com>
2023-12-03 19:01:30 +0900

    feat: detail ui edit

--- src/pages/detail/ReservationModal.jsx
+++ src/pages/detail/ReservationModal.jsx
@@ -1,145 +1,185 @@
 import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
-import { Box, Typography, Button } from '@mui/material'
-import CloseIcon from '../../assets/svg/CloseIcon'
+import { Modal, Box, Typography, Button, Paper, TextField } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import { makeReservation } from '../../fetch/fetchLodging'
+import { getUser } from '../../fetch/fetchLodging'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
-const ReservationModal = ({ lodgingData, closeModal, reservations }) => {
-  // 더미 데이터
-  const roomTypeData = {
-    name: 'Deluxe Sea View', // 객실 이름
-    types: 'Deluxe', // 유형
-    bedType: 'King Size', // 침대 유형
-    price: 200000, // 1박 요금
-    capacity: 2, // 수용 가능 인원
-    size: 35, // 방의 크기 (예: 평방미터)
-    image: [
-      'https://example.com/images/room1.jpg', // 객실 사진 URL
-      'https://example.com/images/room2.jpg',
-    ],
-    amenities: [
-      'Free WiFi', // 제공되는 편의 시설
-      'Air Conditioning',
-      'Mini Bar',
-    ],
-  }
-
-  const roomBookingData = {
-    status: true,
-    checkInDate: '2023-12-01',
-    checkOutDate: '2023-12-05',
-    adults: 2,
-    children: 1,
-    bookingStatus: 'confirmed',
-  }
-
-function ReservationModal({ closeModal, lodgingData, reservations }) {
-  const checkInDate = dayjs(reservations[0].checkInDate)
-  const checkOutDate = dayjs(reservations[0].checkOutDate)
+const ReservationModal = ({
+  lodgingData,
+  closeModal,
+  selectedRoom,
+  selectedRoomType,
+  selectedDates,
+}) => {
+  const checkInDate = dayjs(selectedDates?.startDate || new Date())
+  const checkOutDate = dayjs(selectedDates?.endDate || new Date())
   const totalNights = checkOutDate.diff(checkInDate, 'day')
-  const pricePerNight = roomTypeData.price
+  const pricePerNight = selectedRoomType?.price || 0
   const totalPrice = pricePerNight * totalNights
 
   const [isReserved, setIsReserved] = useState(false)
+  const [userData, setUserData] = useState(null)
 
-  const handleReservation = () => {
-    setIsReserved(true)
+  const fetchUserData = async () => {
+    try {
+      const userData = await getUser()
+      console.log('userData', userData)
+      setUserData(userData)
+    } catch (error) {
+      console.error('Failed to fetch user data:', error)
+    }
   }
 
   useEffect(() => {
-    // 모달이 열릴 때
-    document.body.style.overflow = 'hidden'
+    fetchUserData()
+  }, []) // useEffect를 사용하여 컴포넌트가 마운트될 때에만 데이터를 불러오도록 변경
 
-    // 클린업 함수: 모달이 닫힐 때
-    return () => {
-      document.body.style.overflow = 'visible'
+  const handleReservation = async () => {
+    if (!selectedRoom || !selectedDates.startDate || !selectedDates.endDate) {
+      Swal.fire({
+        title: '선택 필요',
+        text: '객실과 일정을 먼저 선택하세요.',
+        icon: 'warning',
+      })
+      return
     }
-  }, [])
+
+    const { value: confirm } = await Swal.fire({
+      title: '예약 확인',
+      text: '예약을 진행하시겠습니까?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: '예, 예약합니다',
+      cancelButtonText: '아니오',
+    })
+
+    if (confirm) {
+      // 예약 정보 객체 생성 (예시)
+      const reservationData = {
+        user: '6567aac0910622e34444b351',
+        firstName: 'wonsik',
+        lastName: 'seo',
+        email: 'test@test.com',
+        phoneNumber: '010-1111-1111',
+        room: selectedRoomType._id,
+        status: false,
+        checkInDate: selectedDates.startDate,
+        checkOutDate: selectedDates.endDate,
+        adults: selectedRoom.roomBooking.adults,
+        children: selectedRoom.roomBooking.children,
+        request: specialRequest,
+      }
+
+      try {
+        const reservationResult = await makeReservation(reservationData)
+
+        console.log(reservationResult)
+
+        if (reservationResult) {
+          setIsReserved(true)
+          // 예약이 성공하면 SweetAlert로 성공 메시지를 띄울 수 있습니다.
+          Swal.fire({
+            title: '예약 완료',
+            text: '예약이 성공적으로 완료되었습니다.',
+            icon: 'success',
+          })
+        }
+      } catch (error) {
+        // Handle the error as needed
+        // 예약이 실패한 경우에도 SweetAlert로 에러 메시지를 띄울 수 있습니다.
+        Swal.fire({
+          title: '예약 실패',
+          text: '예약을 처리하는 중에 오류가 발생했습니다.',
+          icon: 'error',
+        })
+      }
+    }
+  }
+
+  console.log(selectedRoom)
+  console.log(selectedRoomType)
+
+  const selectedRoomData = selectedRoom || { startDate: '', endDate: '' }
+  const selectedRoomTypeData = selectedRoomType || { price: 0, capacity: 1 }
+
+  const [specialRequest, setSpecialRequest] = useState('')
 
   return (
-    <Box className="fixed inset-0 bg-black bg-opacity-50 overflow-y-hidden h-full w-full flex justify-center items-center z-50">
-      <Box
-        className="bg-white p-4 md:p-8 rounded-lg shadow-2xl max-w-md mx-auto"
-        style={{ borderRadius: 10 }}
-      >
-        <Box className="text-center space-y-4">
-          <Typography
-            variant="h6"
-            className="font-bold text-gray-900"
-            style={{ fontWeight: 'bold' }}
+    <div
+      class="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md"
+      style={{ marginTop: '20px', marginBottom: '20px' }}
+    >
+      <div class="p-5">
+        <div class="flex justify-between items-center">
+          <span class="text-3xl font-bold text-gray-900">
+            ₩{selectedRoomTypeData.price}
+          </span>
+        </div>
+        <div class="flex justify-between items-center mt-4">
+          <div>
+            <span class="block text-sm text-gray-600">체크인</span>
+            <span class="block text-lg text-gray-800">
+              {selectedRoomData.startDate}
+            </span>
+          </div>
+          <div>
+            <span class="block text-sm text-gray-600">체크아웃</span>
+            <span class="block text-lg text-gray-800">
+              {selectedRoomData.endDate}
+            </span>
+          </div>
+        </div>
+        <div class="mt-4">
+          <label for="guests" class="text-sm text-gray-600">
+            인원
+          </label>
+          <select
+            id="guests"
+            class="block w-full mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           >
-            예약 요청
-          </Typography>
+            <option>게스트 {selectedRoomTypeData.capacity}명</option>
+          </select>
+        </div>
+        <div className="mt-4">
+          <label htmlFor="specialRequest" className="text-sm text-gray-600">
+            요청사항
+          </label>
+          <textarea
+            id="specialRequest"
+            value={specialRequest}
+            onChange={(e) => setSpecialRequest(e.target.value)}
+            className="block w-full mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            rows="4" // 원하는 높이로 조정 가능
+            placeholder="특별한 요청사항을 입력하세요"
+          ></textarea>
+        </div>
+        <button
+          onClick={handleReservation}
+          className="w-full px-4 py-3 mt-8 text-sm font-bold text-white uppercase bg-red-500 rounded"
+        >
+          예약하기
+        </button>
 
-          <Box className="flex items-center">
-            <img
-              src={lodgingData.image[0]}
-              alt="객실 이미지"
-              className="max-w-full mx-auto rounded-lg"
-              style={{ maxWidth: '30%', height: 'auto', borderRadius: 10 }}
-            />
-            <Typography className="text-3xl font-bold text-gray-900 ml-4">
-              {lodgingData.name}, {lodgingData.address}
-            </Typography>
-          </Box>
-          <Box className="space-y-2">
-            <Box>
-              <Typography
-                className="text-gray-800"
-                style={{ fontWeight: 'bold' }}
-              >
-                날짜
-              </Typography>
-              <Typography className="text-gray-800">
-                {checkInDate.format('YYYY.MM.DD')}~
-                {checkOutDate.format('YYYY.MM.DD')}
-              </Typography>
-            </Box>
-            <Box>
-              <Typography
-                className="text-gray-800"
-                style={{ fontWeight: 'bold' }}
-              >
-                인원
-              </Typography>
-              <Typography className="text-gray-800">
-                성인 {roomBookingData.adults} / 아이{roomBookingData.children}
-              </Typography>
-            </Box>
-          </Box>
-          {!isReserved ? (
-            <Button
-              onClick={handleReservation}
-              variant="contained"
-              color="primary"
-              className="w-full rounded-full transition duration-300 ease-in-out hover:bg-pink-700"
-            >
-              예약하기
-            </Button>
-          ) : (
-            <Box className="text-lg text-green-700 bg-green-100 py-2 px-4 rounded-md border border-green-200">
-              예약되었습니다!
-            </Box>
-          )}
-          <Typography className="text-sm text-gray-500">
-            예약 확정 전에는 요금이 청구되지 않습니다.
-          </Typography>
-          <Box className="pt-4 border-t-2">
-            <Typography className="text-lg font-semibold">
-              ₩{pricePerNight.toLocaleString()} x {totalNights}박
-            </Typography>
-            <Typography className="text-xl font-bold">
-              총합계 ₩{totalPrice.toLocaleString()}
-            </Typography>
-          </Box>
-          <button
-            onClick={closeModal}
-            className="absolute top-0 right-0 mt-4 mr-4 text-gray-400 hover:text-gray-600"
-          >
-            <CloseIcon />
-          </button>
-        </Box>
-      </Box>
-    </Box>
+        <p class="mt-4 text-xs text-center text-gray-600">
+          예약 확정 전에는 요금이 청구되지 않습니다.
+        </p>
+      </div>
+      <div class="px-5 py-4 bg-gray-100">
+        <div class="flex justify-between">
+          <span>
+            ₩{selectedRoomTypeData.price} x {totalNights}박
+          </span>
+          <span>₩{totalPrice}</span>
+        </div>
+        <div class="flex justify-between mt-4 font-bold">
+          <span>총 합계</span>
+          <span>₩{totalPrice}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 

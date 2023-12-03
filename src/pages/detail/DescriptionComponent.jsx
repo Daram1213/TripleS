63a4e15b523741d48a8bb3af296e111cb21fcfd1
hido <bestdohee@gmail.com>
2023-12-03 19:01:30 +0900

    feat: detail ui edit

--- src/pages/detail/DescriptionComponent.jsx
+++ src/pages/detail/DescriptionComponent.jsx
@@ -1,8 +1,4 @@
 import React, { useState } from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Modal from '@mui/material/Modal'
-import Button from '@mui/material/Button'
 import {
   CheckCircleIcon,
   InformationCircleIcon,
@@ -27,125 +23,78 @@ const DescriptionComponent = ({ lodgingData }) => {
     }
   }
   return (
-    <Box sx={{ maxWidth: 'lg', mx: 'auto', p: 4 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          mb: 4,
-        }}
-      >
-        <Box>
-          <Typography variant="h5" component="h1" sx={{ fontWeight: 'bold' }}>
-            Ddnayo 나이 호스팅하는 펜션
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
+    <div className="max-w-lg mx-auto p-4">
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <h1 className="text-xl font-semibold">Ddnayo 나이 호스팅하는 펜션</h1>
+          <p className="text-sm text-gray-600">
             차대 일명 · 체험 1개 · 손님 1명
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            borderRadius: '50%',
-            bgcolor: 'pink',
-            height: 48,
-            width: 48,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography sx={{ color: 'white', fontSize: 'large' }}>dd</Typography>
-        </Box>
-      </Box>
+          </p>
+        </div>
+        <div className="rounded-full bg-pink-600 h-12 w-12 flex items-center justify-center">
+          {/* Replace with logo */}
+          <span className="text-white text-lg">dd</span>
+        </div>
+      </div>
 
-      <Box sx={{ mb: 4 }}>
+      <div className="space-y-2 mb-4">
         {lodgingData.lodging.option.map((option, index) => (
-          <Box key={index} sx={{ display: 'flex', alignItems: 'center' }}>
-            {getIcon(option.category)}
-            <Typography variant="body2" sx={{ ml: 2 }}>
-              {option.details}
-            </Typography>
-          </Box>
+          <div key={index} className="flex items-center">
+            {getIcon(option.category)}{' '}
+            {/* Replace `option.type` with the actual property that determines the icon */}
+            <p className="ml-2 text-sm">{option.details}</p>
+          </div>
         ))}
-      </Box>
+      </div>
 
-      <Box sx={{ bgcolor: 'grey.100', p: 4 }}>
-        <Typography variant="body2" sx={{ mb: 2 }}>
-          특별한 정보는 자동 번역되었습니다.
-          <Typography
-            component="span"
-            sx={{ color: 'blue', cursor: 'pointer' }}
+      <div className="bg-gray-100 p-4">
+        <p className="text-sm mb-2">
+          특별한 정보는 자동 번역되었습니다.{' '}
+          <span className="text-blue-600 cursor-pointer">원문 보기</span>
+        </p>
+        <p className="text-sm mb-2">{lodgingData.lodging.rule}</p>
+        <p className="text-sm">{lodgingData.lodging.description}</p>
+        <div className="mt-4">
+          <button
+            className="text-sm bg-gray-200 rounded-full px-3 py-1"
+            onClick={() => setShowModal(true)}
           >
-            원문 보기
-          </Typography>
-        </Typography>
-        <Typography variant="body2" sx={{ mb: 2 }}>
-          {lodgingData.lodging.rule}
-        </Typography>
-        <Typography variant="body2">
-          {lodgingData.lodging.description}
-        </Typography>
-        <Button
-          sx={{ mt: 4 }}
-          variant="outlined"
-          onClick={() => setShowModal(true)}
-        >
-          더 보기
-        </Button>
-      </Box>
+            더 보기
+          </button>
+        </div>
+      </div>
 
       {/* Modal for more information */}
-      <Modal
-        open={showModal}
-        onClose={() => setShowModal(false)}
-        sx={{ display: 'flex', alignItems: 'start', justifyContent: 'center' }}
-      >
-        <Box
-          sx={{
-            p: 5,
-            border: 1,
-            borderRadius: 1,
-            bgcolor: 'background.paper',
-            width: '90%',
-            maxWidth: 'md',
-            mt: 10,
-          }}
+      {showModal && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-start"
+          onClick={() => setShowModal(false)}
         >
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              mb: 4,
-            }}
+          <div
+            className="relative top-0 mx-auto p-5 border w-11/12 md:max-w-2xl shadow-lg rounded-md bg-white mt-10"
+            onClick={(e) => e.stopPropagation()} // Prevent click from closing modal
           >
-            <Typography variant="h6" component="h3">
-              숙소 설명
-            </Typography>
-            <Button onClick={() => setShowModal(false)}>
-              <XIcon />
-            </Button>
-          </Box>
-          <Typography variant="body2" sx={{ mb: 2 }}>
-            {lodgingData.lodging.description}
-          </Typography>
-          <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>
-            숙소
-          </Typography>
-          <Typography variant="body2" sx={{ mb: 2 }}>
-            {lodgingData.lodging.theme}
-          </Typography>
-          <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>
-            운영 시간
-          </Typography>
-          <Typography variant="body2" sx={{ mb: 2 }}>
-            {lodgingData.lodging.rule}
-          </Typography>
-          {/* ... additional content ... */}
-        </Box>
-      </Modal>
-    </Box>
+            <div className="flex justify-between items-center mb-4">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                숙소 설명
+              </h3>
+              <button
+                className="rounded-full p-1"
+                onClick={() => setShowModal(false)}
+              >
+                <XIcon className="h-6 w-6" />
+              </button>
+            </div>
+            <p className="text-sm mb-2">{lodgingData.lodging.description}</p>
+            <h2 className="text-lg font-bold mb-2">숙소</h2>
+            <p className="text-sm mb-2">{lodgingData.lodging.theme}</p>
+            <h2 className="text-lg font-bold mb-2">운영 시간</h2>
+            <p className="text-sm mb-2">{lodgingData.lodging.rule}</p>
+            {/* ... additional content ... */}
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 

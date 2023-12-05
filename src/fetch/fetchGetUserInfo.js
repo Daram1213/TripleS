9a4ab26b6271c102c9d0d2b475e98b2765014caf
김나영 <newpaper.nana0@gmail.com>
2023-12-06 01:16:53 +0900

    feat: mypage api 수정및 예약확인 만들기

--- src/fetch/fetchGetUserInfo.js
+++ src/fetch/fetchGetUserInfo.js
@@ -15,22 +15,18 @@ const fetchGetUserInfo = async () => {
   try {
     const accessToken = getCookie('accessToken')
     if (!accessToken) {
-      console.log('accessToken is not available')
       return null
     }
     const accessTokenObject = JSON.parse(accessToken)
     const tokenValue = accessTokenObject.token
-    console.log('tokenValue', tokenValue)
     const response = await axios.get('http://15.165.25.34:3000/api/users', {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${tokenValue}`,
       },
     })
-    console.log(response)
     return response.data
   } catch (error) {
-    console.error('API Error:', error)
     throw new Error('Failed to fetch user data')
   }
 }
--- src/pages/main/Mypage/MemberInfo.jsx
+++ src/pages/main/Mypage/MemberInfo.jsx
@@ -1,80 +1,101 @@
+import { useState, useEffect } from 'react'
 import { Button } from '@mui/base'
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
+  TextField,
+  Container,
+  Typography,
+  Box,
+  InputLabel,
 } from '@mui/material'
+import Swal from 'sweetalert2'
 import fetchGetUserInfo from '../../../fetch/fetchGetUserInfo'
 import fetchUpdateUser from '../../../fetch/fetchUpdateUser'
 
-function createData(name, calories, fat) {
-  return { name, calories, fat }
-}
-
-const rows = [createData('Frozen yoghurt', 159, 6.0, 24, 4.0)]
-
 export default function MemberInfo() {
-  const handleGetUserInfo = async (e) => {
-    e.preventDefault()
-    const result = await fetchGetUserInfo()
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+  })
 
-    if (result) {
-      console.log(result.data)
+  const handleGetUserInfo = async () => {
+    try {
+      const result = await fetchGetUserInfo()
+      if (result) {
+        setUserInfo({
+          name: result.data.name,
+          email: result.data.email,
+        })
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: '오류 발생',
+        text: '회원 정보 조회 중 오류가 발생했습니다.',
+      })
     }
   }
 
+  useEffect(() => {
+    handleGetUserInfo()
+  }, [])
+
   const handleUpdateUserInfo = async (e) => {
     e.preventDefault()
-    const result = await fetchUpdateUser()
 
-    if (result) {
-      console.log(result.data)
+    try {
+      const result = await fetchUpdateUser({
+        name: userInfo.name,
+        email: userInfo.email,
+      })
+
+      if (result) {
+        Swal.fire({
+          icon: 'success',
+          title: '회원 정보 업데이트 성공',
+          text: '회원 정보가 성공적으로 업데이트되었습니다.',
+        })
+        handleGetUserInfo()
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: '오류 발생',
+        text: '회원 정보 업데이트 중 오류가 발생했습니다.',
+      })
     }
   }
 
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>이름</TableCell>
-              <TableCell align="right">이메일</TableCell>
-              <TableCell align="right">이름</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Button
-        onClick={handleGetUserInfo}
-        className="bg-blue-600 text-white mt-[2rem] rounded-md p-2 w-1/5"
-      >
-        조회
-      </Button>
+    <Container maxWidth="sm">
+      <Typography variant="h5" gutterBottom>
+        My 계정
+      </Typography>
+      <Box className="mt-[2rem]">
+        <InputLabel htmlFor="name">이름</InputLabel>
+        <TextField
+          fullWidth
+          id="name"
+          variant="outlined"
+          margin="normal"
+          value={userInfo.name}
+          onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
+        />
+        <InputLabel htmlFor="email">이메일</InputLabel>
+        <TextField
+          fullWidth
+          id="email"
+          variant="outlined"
+          margin="normal"
+          value={userInfo.email}
+          onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+        />
+      </Box>
       <Button
         onClick={handleUpdateUserInfo}
-        className="bg-blue-600 text-white ml-[1rem] mt-[2rem] rounded-md p-2 w-1/5"
+        className="bg-blue-600 text-white mt-[2rem] rounded-md p-2 w-full"
       >
         수정
       </Button>
-    </>
+    </Container>
   )
 }

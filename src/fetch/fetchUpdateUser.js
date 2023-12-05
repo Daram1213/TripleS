5fef26a0d1a31d9e05d2f0997bde886e577e3b2c
김나영 <newpaper.nana0@gmail.com>
2023-12-05 19:10:30 +0900

    fix: mypage api headers token추가

--- src/fetch/fetchUpdateUser.js
+++ src/fetch/fetchUpdateUser.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
 
-const fetchFindUser = async (email, password, name) => {
+function getCookie(name) {
+  const cookies = document.cookie.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim()
+    if (cookie.startsWith(`${name}=`)) {
+      return cookie.substring(name.length + 1)
+    }
+  }
+  return null
+}
+
+const fetchUpdateUser = async (email, password, name) => {
   try {
+    const accessToken = getCookie('accessToken')
+    if (!accessToken) {
+      console.log('accessToken is not available')
+      return null
+    }
+    const accessTokenObject = JSON.parse(accessToken)
+    const tokenValue = accessTokenObject.token
+    console.log('tokenValue', tokenValue)
     const response = await axios.put(
       'http://15.165.25.34:3000/api/users',
       {
@@ -12,13 +31,16 @@ const fetchFindUser = async (email, password, name) => {
       {
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${tokenValue}`,
         },
       },
     )
+    console.log(response)
     return response.data
   } catch (error) {
-    throw new Error('fetchUpdateUser Error')
+    console.error('API Error:', error)
+    throw new Error('Failed to fetch user data')
   }
 }
 
-export default fetchFindUser
+export default fetchUpdateUser

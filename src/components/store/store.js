d33a364456b362eec4ee1b3df6ba3d63ff076d0e
jay <valenqbd02@gmail.com>
2023-12-06 17:04:45 +0900

    feat: store에서 api호출, fetchAddLikes, fetchDeleteLikes

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,7 +1,10 @@
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
+import { useQuery } from 'react-query'
+import fetchAddLikes from '../../fetch/fetchAddLikes'
+import fetchDeleteLikes from '../../fetch/fetchDeleteLikes'
 
-const store = (set) => ({
+const store = (set, get) => ({
   likedAttractions: [],
   // 로그인시 유저 정보 조회 후 setLikedAttraction(data.favoriteAttractions)
   setLikedAttraction: (fetchedAttractions) =>
@@ -12,32 +15,48 @@ const store = (set) => ({
       false,
       'setLikedAttraction',
     ),
-  addLikedAttraction: (attraction) =>
-    set(
-      (state) => {
-        const attractionExists = state.likedAttractions.some(
-          (item) => item.attractionId === attraction.attractionId,
-        )
-        if (!attractionExists) {
-          return {
+  addLikedAttraction: async (attraction) => {
+    try {
+      const attractionExists = get().likedAttractions.some(
+        (item) => item.attractionId === attraction.attractionId,
+      )
+      if (attractionExists) {
+        return
+      }
+
+      const response = await fetchAddLikes(attraction.attractionId)
+
+      if (response) {
+        set(
+          (state) => ({
             likedAttractions: [...state.likedAttractions, attraction],
-          }
-        }
-        return state
-      },
-      false,
-      'addLikedAttraction',
-    ),
-  deleteLikedAttraction: (id) =>
-    set(
-      (state) => ({
-        likedAttractions: state.likedAttractions.filter(
-          (attraction) => attraction.attractionId !== id,
-        ),
-      }),
-      false,
-      'deleteLikedAttraction',
-    ),
+          }),
+          false,
+          'addLikedAttraction',
+        )
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  deleteLikedAttraction: async (attractionId) => {
+    try {
+      const response = await fetchDeleteLikes(attractionId)
+      if (response) {
+        set(
+          (state) => ({
+            likedAttractions: state.likedAttractions.filter(
+              (attraction) => attraction.attractionId !== attractionId,
+            ),
+          }),
+          false,
+          'deleteLikedAttraction',
+        )
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
 })
 
 export const useStore = create(persist(devtools(store), { name: 'store' }))
